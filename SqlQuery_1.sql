42aa5f4c7d1f4a983dac9aeea9685066aa377149
cole.giannotti <colegiannotti@gmail.com>
2023-10-27 12:21:54 -0400

    Commit

---
+++ SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿SELECT Orders.OrderID, Customers.CustomerName
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID
+WHERE OrderID = 10310;
+
+SELECT Products.ProductID, Suppliers.Address
+From Products
+INNER JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+WHERE ProductID = 40;
